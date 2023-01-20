02456d5a40359aaa8c39aa61b567777b879cc561
KATT <alexander@n1s.se>
2023-01-20 09:52:39 +0100

    add failing test
    #3630

--- packages/tests/server/router.test.ts
+++ packages/tests/server/router.test.ts
@@ -3,35 +3,43 @@ import { initTRPC } from '@trpc/server/src/core';
 
 const t = initTRPC.create();
 
-describe('router', () => {
-  test('is a reserved word', async () => {
-    expect(() => {
-      return t.router({
-        then: t.procedure.query(() => 'hello'),
-      });
-    }).toThrowErrorMatchingInlineSnapshot(
-      `"Reserved words used in \`router({})\` call: then"`,
-    );
-  });
+test('"then" is a reserved word', async () => {
+  expect(() => {
+    return t.router({
+      then: t.procedure.query(() => 'hello'),
+    });
+  }).toThrowErrorMatchingInlineSnapshot(
+    `"Reserved words used in \`router({})\` call: then"`,
+  );
+});
 
-  // Regression https://github.com/trpc/trpc/pull/2562
-  test('because it creates async fns that returns proxy objects', async () => {
-    const appRouter = t.router({});
-    const asyncFnThatReturnsCaller = async () => appRouter.createCaller({});
+// Regression https://github.com/trpc/trpc/pull/2562
+test('because it creates async fns that returns proxy objects', async () => {
+  const appRouter = t.router({});
+  const asyncFnThatReturnsCaller = async () => appRouter.createCaller({});
 
-    await asyncFnThatReturnsCaller();
-  });
+  await asyncFnThatReturnsCaller();
+});
 
-  test('should not duplicate key', async () => {
-    expect(() =>
-      t.router({
-        foo: t.router({
-          '.bar': t.procedure.query(() => 'bar' as const),
-        }),
-        'foo.': t.router({
-          bar: t.procedure.query(() => 'bar' as const),
-        }),
+test('should not duplicate key', async () => {
+  expect(() =>
+    t.router({
+      foo: t.router({
+        '.bar': t.procedure.query(() => 'bar' as const),
       }),
-    ).toThrow('Duplicate key: foo..bar');
+      'foo.': t.router({
+        bar: t.procedure.query(() => 'bar' as const),
+      }),
+    }),
+  ).toThrow('Duplicate key: foo..bar');
+});
+
+test.only('short-hand router', async () => {
+  const router = t.router({
+    foo: {
+      bar: t.procedure.query(() => 'bar'),
+    },
   });
+
+  expect(await router.createCaller({}).foo.bar()).toBe('bar');
 });
